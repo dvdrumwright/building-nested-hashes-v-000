e567292750b804543eae0e9ce197c5c6b29b258d
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-04-19 01:06:59 +0000

    automatically backed up by learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -25,13 +25,13 @@ def bonus
   epic_tragedy[:montague][:hero] 
   [:status] = "dead" 
   
-  
+  puts epic_tragedy
 
   
   epic_tragedy[:capulet][:heroine]
   [:status] = "dead"
 
-  
+  puts epic_tragedy
   
   
 
